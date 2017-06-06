9bc8d5fd432bc8745d5bd902bd22a846a4b71641
razvanb <gabriel.razvann@yahoo.com>
2017-06-06 13:22:29 +0300

    first commit

---
+++ src/IntercalateTwoStrings.java
@@ -0,0 +1,9 @@
+/**
+ * Created by razvanb on 06.06.2017.
+ */
+public class IntercalateTwoStrings {
+
+    public static void main(String[] args) {
+        System.out.print("This is a test");
+    }
+}
